97014c9978676619aabdfddfabe56d431361d414
buianh <buimanh1505@gmail.com>
2020-07-23 23:41:50 -0700

    lab14

--- lab14/RedBlackTree.java
+++ lab14/RedBlackTree.java
@@ -23,13 +23,34 @@ public class RedBlackTree<T extends Comparable<T>> {
 
         if (r.getItemCount() == 1) {
             // TODO: Replace with code to create a 2 node equivalent
-            return null
+            RBTreeNode rbnode = new RBTreeNode(true, r.getItemAt(0));
+            if (r.getChildrenCount() != 0) {
+                rbnode.left = buildRedBlackTree(r.getChildAt(0));
+                rbnode.right = buildRedBlackTree(r.getChildAt(1));
+            }
+            return rbnode;
         } else if (r.getItemCount() == 2) {
             // TODO: Replace with code to create a 3 node equivalent
-            return null
+            RBTreeNode rbnode = new RBTreeNode(true, r.getItemAt(1));
+            rbnode.left = new RBTreeNode(false, r.getItemAt(0));
+            if (r.getItemCount() != 0) {
+                rbnode.left.left = buildRedBlackTree(r.getChildAt(0));
+                rbnode.left.right = buildRedBlackTree(r.getChildAt(1));
+                rbnode.right = buildRedBlackTree(r.getChildAt(2));
+            }
+            return rbnode;
         } else {
             // TODO: Replace with code to create a 4 node equivalent
-            return null
+            RBTreeNode rbnode = new RBTreeNode(true, r.getItemAt(1));
+            rbnode.left = new RBTreeNode(false, r.getItemAt(0));
+            rbnode.right = new RBTreeNode(false, r.getItemAt(2));
+            if (r.getChildrenCount() != 0) {
+                rbnode.left.left = buildRedBlackTree(r.getChildAt(0));
+                rbnode.left.right = buildRedBlackTree(r.getChildAt(1));
+                rbnode.right.left = buildRedBlackTree(r.getChildAt(2));
+                rbnode.right.right = buildRedBlackTree(r.getChildAt(3));
+            }
+            return rbnode;
         }
     }
 
@@ -45,15 +66,22 @@ public class RedBlackTree<T extends Comparable<T>> {
        this subtree. */
     RBTreeNode<T> rotateRight(RBTreeNode<T> node) {
         // TODO: YOUR CODE HERE
-        return null;
+        RBTreeNode rbnode_right = new RBTreeNode(false,node.item,node.left.right, node.right);
+        RBTreeNode rbnode = new RBTreeNode(node.isBlack,node.left.item, node.left.left,rbnode_right);
+
+        return rbnode;
     }
 
     /* Rotates the given node NODE to the left. Returns the new root node of
        this subtree. */
     RBTreeNode<T> rotateLeft(RBTreeNode<T> node) {
         // TODO: YOUR CODE HERE
-        return null;
+        RBTreeNode rbnode_left = new RBTreeNode(false,node.item,node.left,node.right.left);
+        RBTreeNode rbnode = new RBTreeNode(node.isBlack,node.right.item,rbnode_left,node.right.right);
+        return rbnode;
     }
+
+
 
     public void insert(T item) {   
         root = insert(root, item);  
@@ -62,7 +90,34 @@ public class RedBlackTree<T extends Comparable<T>> {
 
     private RBTreeNode<T> insert(RBTreeNode<T> node, T item) {
         // TODO: YOUR CODE HERE
-        return null;
+        if (node==null){
+            node = new RBTreeNode<>(false,item);
+        }
+        //Recursively adds item:
+        int comp = item.compareTo(node.item);
+        if (comp>0){
+            insert(node.right,item);
+        } else if (comp <0){
+            insert(node.left,item);
+        } else {
+            return node;
+        }
+
+        //Adjustments for 2-3 Tree, i.e. left-leaning red black tree
+        //in the end, node is the new red node added
+        if (!isRed(node)&&isRed(node.right)) {
+            rotateLeft(node);
+        }
+        if (isRed(node.left)&&isRed(node.right)) {
+            flipColors(node);
+        }
+        if (isRed(node.left)&& isRed(node.left.left)){
+            rotateRight(node);
+        }
+        if (isRed(node.left)&&isRed(node.left.right)){
+            rotateLeft(node.left);
+        }
+    return node;
     }
 
     /* Returns whether the given node NODE is red. Null nodes (children of leaf
