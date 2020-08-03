4e2f058f78bbd34e004f78f05800d67fd41bff0c
buianh <buimanh1505@gmail.com>
2020-08-03 11:06:21 -0700

    lab20

--- lab20/UnionFind.java
+++ lab20/UnionFind.java
@@ -1,30 +1,42 @@
 public class UnionFind {
 
     /* TODO: Add instance variables here. */
+    int[] djs;
+    int capacity;
 
     /* Creates a UnionFind data structure holding N vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int N) {
         // TODO: YOUR CODE HERE
+        capacity = N;
+        djs = new int[capacity];
+        for (int i=0;i<capacity;i++){
+            djs[i]=-1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        return -djs[find(v)];
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        int parent = find(v);
+        if (parent >= 0) {
+            return parent;
+        } else {
+            return djs[parent];
+        }
     }
 
     /* Returns true if nodes V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO: YOUR CODE HERE
-        return false;
+        return find(v1)==find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
@@ -32,7 +44,17 @@ public class UnionFind {
        function, throw an IllegalArgumentException. */
     public int find(int v) {
         // TODO: YOUR CODE HERE
-        return -1;
+        if (v<0||v>=capacity){
+            throw new IllegalArgumentException();
+        }
+        int parent = djs[v];
+        if (parent < 0) {
+            parent=v;
+        }
+        while (parent>=0){
+            find(parent);
+        }
+        return parent;
     }
 
     /* Connects two elements V1 and V2 together. V1 and V2 can be any element,
@@ -42,5 +64,16 @@ public class UnionFind {
        structure. */
     public void union(int v1, int v2) {
         // TODO: YOUR CODE HERE
+        if (!connected(v1,v2)){
+            int v1_size = sizeOf(v1);
+            int v2_size = sizeOf(v2);
+            if (v1_size>= v2_size){
+                djs[find(v2)]=find(v1);
+                djs[find(v1)]-= sizeOf(v2);
+            } else {
+                djs[find(v1)]=find(v2);
+                djs[find(v2)]-= sizeOf(v1);
+            }
+        }
     }
 }
